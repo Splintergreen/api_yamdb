58c74b3586b312065d737eea83ad2f96d9a3b070
Сухоцкий Станислав <ssuhotskiy@yandex.ru>
2022-10-21 21:28:53 +0300

    add pagination fix redme

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -115,6 +115,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
 }
 
 # EMAIL send configuration
